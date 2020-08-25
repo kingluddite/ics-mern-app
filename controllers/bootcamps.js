bf9af36e8712267276c9728c96840ee9dd1d7a92
Kingluddite <howley.phil@gmail.com>
2020-08-25 12:34:26 -0400

    Add bootcamps CRUD

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,18 +1,39 @@
+const Bootcamp = require('../models/Bootcamp');
+
 // @desc     Get bootcamp
-// @route    GET /api/v1/bootcamps
+// @route    GET /api/v1/bootcamps/:id
 // @access   Public
-exports.getBootcamp = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    msg: 'Show a bootcamp',
-    error: null,
-  });
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const { id: bootcampIdReq } = req.params;
+    const bootcamp = await Bootcamp.findById(bootcampIdReq);
+    // search for a correctly formatted id but doesn't exist in our Database generate an error
+    if (!bootcamp) {
+      // IMPORTANT! You can't send 2 responses so you need to return the first one so it won't go to the 2nd one
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc     Get all bootcamps
 // @route    GET /api/v1/bootcamps
 // @access   Public
-exports.getBootcamps = (req, res, next) => {
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+
+    // Helper info too
+    // When we get all bootcamps let's also get back the length of the bootcamps
+    res
+      .status(200)
+      .json({ success: true, count: bootcamps.length, data: bootcamps });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
   res.status(200).json({
     success: true,
     msg: 'Show all bootcamps',
@@ -24,32 +45,70 @@ exports.getBootcamps = (req, res, next) => {
 // @desc    Create a bootcamp
 // @route   POST /api/v1/bootcamps
 // @access  Private
-exports.createBootcamp = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    msg: 'Create a bootcamp',
-    error: null,
-  });
+exports.createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+
+    return res.status(201).json({
+      success: true,
+      msg: 'Create a bootcamp',
+      data: bootcamp,
+      error: null,
+    });
+  } catch (err) {
+    // Above is the easiest way to handle errors
+    // - But we'll refactor to make our code more efficient and save us time when we add new things
+    console.error(err.message);
+    // 400 means client error ---> they sent bad request
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc    Update a bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
-exports.updateBootcamp = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    msg: `Update a bootcamp with ${req.params.id}`,
-    error: null,
-  });
+exports.updateBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+      // when we get our response we want the data to be the **updated** data
+      new: true,
+      // run our mongoose validators on update
+      runValidators: true,
+    });
+
+    // make sure bootcamp exists
+    if (!bootcamp) {
+      // return to prevent "headers already sent error"
+      return res.status(400).json({ success: false });
+    }
+
+    // We only send a status of `200` (not 201)
+    // because we are not creating a new resource just modifying an existing resource
+    res.status(200).json({
+      success: true,
+      msg: `Update a bootcamp with ${req.params.id}`,
+      error: null,
+      data: bootcamp,
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc    Delete a bootcamp
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  Private
-exports.deleteBootcamp = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    msg: `Delete a bootcamp with ${req.params.id}`,
-    error: null,
-  });
+exports.deleteBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
--- server.js
+++ server.js
@@ -67,17 +67,18 @@ app.use('/api/v1/bootcamps', bootcamps);
 // Define port remotely and locally
 const PORT = process.env.PORT || 5000;
 
-
 // So we can close the server and stop our app if we get this unhandled rejection
 // We do this because if our Database isn't working we don't want our app to work
 const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red);
 
   // Close server & exit process
   server.close(() => process.exit(1));
